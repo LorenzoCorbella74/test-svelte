04658b40bbec3a10e61669eb6c358e62c443b161
Lorenzo Corbella <lcorbella@expertsystem.local>
2020-07-09 16:29:28 +0200

    readme + update + style

--- src/App.svelte
+++ src/App.svelte
@@ -29,7 +29,7 @@
     { label: "Harmonic minor", value: "harmonic" },
     { label: "Melodic minor", value: "melodic" }
   ];
-  let choice = {};
+  let base = {};
   let relations = {
     root: form.key,
     chords: null,
@@ -37,30 +37,35 @@
   };
   let chords = [];
 
+  let editMode = false;
+  let updateIndex  = null;
+
   onMount(() => {
     relations.selectedChord = relations.chords[0];
   });
 
+  // quando cambia form.key / form.scale
   $: {
     let { natural, harmonic, melodic } = Key.minorKey(form.key);
     let major = Key.majorKey(form.key);
     let all = { major, natural, harmonic, melodic };
-    choice = all[form.scale];
-    console.log(choice);
+    base = all[form.scale];
+    console.log(base);
   }
 
+  // quando cambia relations.root
   $: {
-    let availableScales = choice.chordScales.map(e => e.split(" ")[1]);
-    let index = choice.scale.findIndex(e => e === relations.root);
+    let availableScales = base.chordScales.map(e => e.split(" ")[1]);
+    let index = base.scale.findIndex(e => e === relations.root);
     relations.chords = Scale.scaleChords(availableScales[index]); // si prendono solo gli accordi della scala
   }
 
-  function addSelected() {
+  function save() {
     if (relations.selectedChord && relations.root) {
       let name = `${relations.root}${relations.selectedChord}`;
       let newChord = Object.assign({}, Chord.get(name), { name });
       newChord.newIntervals = [];
-      newChord.newNotes = choice.scale.map((e, i) => {
+      newChord.newNotes = base.scale.map((e, i) => {
         if (newChord.notes.includes(e)) {
           return e;
         } else {
@@ -76,7 +81,17 @@
         }
       });
       console.log(newChord);
-      chords = [...chords, newChord];
+      if (editMode) {
+		  chords = [
+            ...chords.slice(0, updateIndex),
+            newChord,
+            ...chords.slice(updateIndex+1)
+        ];
+		editMode = false;
+		updateIndex = null;
+      } else {
+        chords = [...chords, newChord];
+      }
       // defauts
       relations.selectedChord = relations.chords[0];
       relations.root = form.key;
@@ -85,6 +100,14 @@
 
   function deleteChord(index) {
     chords = chords.filter((e, i) => i !== index);
+  }
+
+  function editChord(index) {
+    let chord = chords.find((e, i) => i === index);
+    relations.selectedChord = chord.name.substr(chord.tonic.length);
+    relations.root = chord.tonic;
+	editMode = true;
+	updateIndex = index;
   }
 </script>
 
@@ -116,7 +139,6 @@
   }
 
   .col {
-    /*  flex: 1; */
     align-self: center;
     margin-right: 0.5rem;
   }
@@ -126,14 +148,27 @@
     flex: 1;
   }
 
+  .muted{
+	  color: #b6b6b6;
+  }
+
+  .action {
+	  color :#47a01a;
+  }
+
+  .delete-btn{
+	  cursor:pointer;
+		  font-size: 1.5rem;
+		  font-weight: 600;
+
+
+  }
+
   table {
-    /*  margin-left: 20%; */
   }
 
   td,
   th {
-    /* border: 1px solid #ddd; */
-    /* padding: 4px 8px; */
     width: 80px;
     text-align: center;
   }
@@ -169,7 +204,7 @@
     <div class="col">
       <h5>Key</h5>
       <select bind:value={relations.root}>
-        {#each choice.scale as key}
+        {#each base.scale as key}
           <option value={key}>{key}</option>
         {/each}
       </select>
@@ -183,7 +218,9 @@
       </select>
     </div>
     <div class="col btn">
-      <button on:click={addSelected} style="width:100%">Add</button>
+      <button on:click={save} style="width:100%; cursor:pointer;">
+        {editMode ? 'Update' : 'Add'}
+      </button>
     </div>
   </div>
 
@@ -191,19 +228,19 @@
     <table>
       <tr>
         <th>Grades</th>
-        {#each choice.grades as grade}
+        {#each base.grades as grade}
           <th>{grade}</th>
         {/each}
       </tr>
       <tr>
         <td>Intervals</td>
-        {#each choice.intervals as interval}
-          <td>{interval}</td>
+        {#each base.intervals as interval}
+          <td class="muted">{interval}</td>
         {/each}
       </tr>
       <tr>
         <td>Notes</td>
-        {#each choice.scale as note}
+        {#each base.scale as note}
           <td>{note}</td>
         {/each}
       </tr>
@@ -216,17 +253,19 @@
         <tr transition:fade>
           <td />
           {#each chord.newIntervals as interval}
-            <td>{interval}</td>
+            <td class="muted">{interval}</td>
           {/each}
         </tr>
         <tr transition:fade>
-          <td>
-            <strong>{chord.name}</strong>
+          <td on:click={() => editChord(index)} style="cursor:pointer;">
+            <strong class="action">{chord.name}</strong>
           </td>
           {#each chord.newNotes as note}
             <td>{note}</td>
           {/each}
-          <td on:click={() => deleteChord(index)}>&times;</td>
+          <td on:click={() => deleteChord(index)} class="action delete-btn">
+            &times;
+          </td>
         </tr>
       {/each}
     </table>
